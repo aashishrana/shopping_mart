eeb2411fdb54d569440a48327b95670a79cdc731
Aashish rana <aashishrana71135@gmail.com>
2023-12-04 22:35:38 +0530

    model problem solved

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
-const uuidv1 = require("uuid/v1");
+// const uuidv1 = require("uuid/v1");
+
+const {v4 : uuidv4} = require('uuid')
+uuidv4();
 
 const userSchema = new mongoose.Schema(
   {
@@ -46,7 +49,7 @@ userSchema
   .virtual("password")
   .set(function(password) {
     this._password = password;
-    this.salt = uuidv1();
+    this.salt = uuidv4();
     this.encry_password = this.securePassword(password);
   })
   .get(function() {
